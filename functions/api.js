f622f277952b2dd93bce6437885e36026b9b45a9
Waves21 <jeromelingon02@gmail.com>
2024-05-23 02:56:25 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 const cloudDB = 'mongodb+srv://rayo:Sekome12345@cluster0.mns7uso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const localDB = 'mongodb://localhost:27017/serverless-api';
+const localDB = 'mongodb://localhost:27017/serverless-api-L1ngon';
 
 app.use(cors());
 app.use(express.json());
